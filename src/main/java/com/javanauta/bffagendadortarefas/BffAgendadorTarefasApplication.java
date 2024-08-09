8a2f2577e77efc3a976c5127012437f0985ce1c3
Angelica <angelicaweilerw@gmail.com>
2024-08-09 08:03:07 -0300

    adicionado Cron

--- src/main/java/com/javanauta/bffagendadortarefas/BffAgendadorTarefasApplication.java
+++ src/main/java/com/javanauta/bffagendadortarefas/BffAgendadorTarefasApplication.java
@@ -5,10 +5,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
-@OpenAPIDefinition(info = @Info(title = "Usuario"))
+@EnableScheduling
 public class BffAgendadorTarefasApplication {
 
 	public static void main(String[] args) {
